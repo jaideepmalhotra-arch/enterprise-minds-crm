adc4dc4edaff6c8cf4a08b8c1874ecf0ff0c0af8
jaideepmalhotra-arch <jaideep.malhotra@gmail.com>
2026-03-28 15:30:45 +0530

    Delete src directory

--- src/data/sheetsConfig.js
+++
@@ -1,6 +0,0 @@
-export const SHEETS_CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw3fjUzgDoRaHXbXi3thmCEZCFdn4PhdWiRmmAAnJHV2qxoZffSiLOlN7DYasn5Iw/exec',
-  SHEET_ID:   '1vdibpwuHZJay3O7eEvJ8axwp33dqkLBEb8tghauTbZU',
-  TABS: { CONTACTS:'Contacts', IMPORTS:'Imports' },
-};
-export const SHEETS_CONNECTED = !!(SHEETS_CONFIG.SCRIPT_URL && SHEETS_CONFIG.SHEET_ID);
